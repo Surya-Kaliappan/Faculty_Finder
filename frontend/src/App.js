88e2fa749282464db5df9b4969ef5527cea252f8
Surya-Kaliappan <155989888+Surya-Kaliappan@users.noreply.github.com>
2025-03-13 01:00:03 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FacultyFinder from "./components/FacultyFinder";
+import "./styles.css";
+
+function App() {
+    return (
+        <div>
+            <FacultyFinder />
+        </div>
+    );
+}
+
+export default App;
